d765f8f83d8ee176e441f9e502c5a3295d2af7a3
DaliusDeveikis <slkyper@gmail.com>
2022-03-28 20:08:39 +0300

    files list

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 import { file } from './lib/file.js';
 
 const createFileStatus = await file.create('users', 'petras.json', {
-  name: 'Petras'
+  name: 'Petras',
+  age: 25
 });
 console.log(createFileStatus);
 
-const readFileStatus1 = await file.read('users', 'petras.json');
+const readFileStatus = await file.read('users', 'petras.json');
+console.log(readFileStatus);
+
+const createFileStatus1 = await file.create('users', 'maryte.json', {
+  name: 'Maryte',
+  age: 22
+});
+console.log(createFileStatus1);
+
+const readFileStatus1 = await file.read('users', 'maryte.json');
 console.log(readFileStatus1);
 
-const updateFileStatus = await file.update('users', 'petras.json', {
-  name: 'Petras',
-  age: 99
-});
-console.log(updateFileStatus);
+const listStatus = await file.list('users');
+console.log(listStatus);
 
-const readFileStatus2 = await file.read('users', 'petras.json');
-console.log(readFileStatus2);
+// const updateFileStatus = await file.update('users', 'petras.json', {
+//   name: 'Petras',
+//   age: 99
+// });
+// console.log(updateFileStatus);
 
-const deleteFileStatus = await file.delete('users', 'petras.json');
-console.log(deleteFileStatus);
+// const readFileStatus2 = await file.read('users', 'petras.json');
+// console.log(readFileStatus2);
 
-const readFileStatus3 = await file.read('users', 'petras.json');
-console.log(readFileStatus3);
+// const deleteFileStatus = await file.delete('users', 'petras.json');
+// console.log(deleteFileStatus);
+
+// const readFileStatus3 = await file.read('users', 'petras.json');
+// console.log(readFileStatus3);
